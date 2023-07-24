2c3e6ba38cca1a36297fc72a67606e961ab8a91d
Steve Walsh <walsh124@vt.edu>
2023-07-24 13:46:44 -0400

    add PNG option, change subtracted mean term

--- two_way_ints.R
+++ two_way_ints.R
@@ -2,6 +2,8 @@
 
 library(fields)
 
+PNG <- T
+
 p = 4
 n_pc = 4  # number of principal components to model
 des <- "FF"
@@ -11,8 +13,6 @@ load(paste0("/projects/precipit/hydro/hydro-SA-",des,if(des=="FF"){n_ff},
             if(des=="unif"){paste0("-",m)},"-nPC",n_pc,"_all64.rda"))
 
 if(nv != n_ff) stop("this dim mismatch is not good for image plots")
-
-pdf(paste0("pdf/two_way_ints_",nv,".pdf"))
 
 # get ranges so they are consistent across all plots
 zr <- c(0,0)
@@ -25,9 +25,9 @@ for (i in 1:(p-1)) {
           two_way_ints[ii,jj] <- mean((bssEmu_meo[,which(facDes[,i] == prange[ii] &
                                                            facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
           two_way_ints1[ii,jj] <- mean((bssEmu[,which(facDes[,i] == prange[ii] &
-                                                           facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
+                                                        facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
           two_way_ints2[ii,jj] <- mean((etaEmu[,which(facDes[,i] == prange[ii] &
-                                                           facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
+                                                        facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
           
         }
       }
@@ -37,6 +37,8 @@ for (i in 1:(p-1)) {
 }
 
 ## BSS-ANOVA (MEs only)
+if(PNG) png("png/two_way_ints_BSS1.png", width=2000,height=2000,res=300)
+par(mfrow=c(6,11), pty="s",oma=c(3,3,3,5),mar=c(0,0,0,0))
 for (i in 1:(p-1)) {
   for (j in (i+1):p) {
     # par(mfrow=c(2,3),oma=c(4,4,1.5,1),mar=c(0,0,0,0))
@@ -45,19 +47,26 @@ for (i in 1:(p-1)) {
       for (ii in 1:nv) {
         for (jj in 1:nv) {
           two_way_ints[ii,jj] <- mean((bssEmu_meo[,which(facDes[,i] == prange[ii] &
-                                                       facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
+                                                           facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
         }
       }
-      image.plot(two_way_ints, xlab = paste("param",i), ylab = paste("param",j),
-                 main = paste("bss-anova (MEs only), smval", sm),
-                 zlim = zr)
+      image(two_way_ints, col=heat.colors(100),
+            zlim = zr,ylab="", yaxt="n",xlab="", xaxt="n", asp=1)
+      contour(two_way_ints, levels = c(-0.5,-0.25,0,.25,.5), add=T)
+      if(sm == 1) mtext(paste(i,j,sep=","), side=2)
     }
   }
 }
-
+par(mfrow=c(1,1),mar=c(0,0,0,0),oma=c(3,15,3,0.5))
+image.plot(matrix(rep(zr,2),2,2), legend.only = T, col=heat.colors(100))
+par(mfrow=c(1,1),oma=c(0,0,0,0))
+mtext("Stellar mass value", side=1, line=-2)
+dev.off()
 
 ### BSS-ANOVA: MEs and 2WIs
 # get ranges so they are consistent across all plots
+if(PNG) png("png/two_way_ints_BSS2.png", width=2000,height=2000,res=300)
+par(mfrow=c(6,11), pty="s",oma=c(3,3,3,5),mar=c(0,0,0,0))
 for (i in 1:(p-1)) {
   for (j in (i+1):p) {
     # par(mfrow=c(2,3),oma=c(4,4,1.5,1),mar=c(0,0,0,0))
@@ -69,15 +78,23 @@ for (i in 1:(p-1)) {
                                                        facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
         }
       }
-      image.plot(two_way_ints, xlab = paste("param",i), ylab = paste("param",j),
-                 main = paste("bss-anova (ME and 2WIs), smval", sm),
-                 zlim = zr)
+      image(two_way_ints, col=heat.colors(100),
+            zlim = zr,ylab="", yaxt="n",xlab="", xaxt="n", asp=1)
+      contour(two_way_ints, levels = c(-0.5,-0.25,0,.25,.5), add=T)
+      if(sm == 1) mtext(paste(i,j,sep=","), side=2)
     }
   }
 }
+par(mfrow=c(1,1),mar=c(0,0,0,0),oma=c(3,15,3,0.5))
+image.plot(matrix(rep(zr,2),2,2), legend.only = T, col=heat.colors(100))
+par(mfrow=c(1,1),oma=c(0,0,0,0))
+mtext("Stellar mass value", side=1, line=-2)
+dev.off()
 
 # GP-PC
 # get ranges so they are consistent across all plots
+if(PNG) png("png/two_way_ints_GPPC.png", width=2000,height=2000,res=300)
+par(mfrow=c(6,11), pty="s",oma=c(3,3,3,5),mar=c(0,0,0,0))
 for (i in 1:(p-1)) {
   for (j in (i+1):p) {
     # par(mfrow=c(2,3),oma=c(4,4,1.5,1),mar=c(0,0,0,0))
@@ -89,12 +106,16 @@ for (i in 1:(p-1)) {
                                                        facDes[,j] == prange[jj])])[sm,]) - avg_mean[sm]
         }
       }
-      image.plot(two_way_ints, xlab = paste("param",i), ylab = paste("param",j),
-                 main = paste("GP-PC, smval", sm),
-                 zlim = zr)
+      image(two_way_ints, col=heat.colors(100),
+            zlim = zr,ylab="", yaxt="n",xlab="", xaxt="n", asp=1)
+      contour(two_way_ints, levels = c(-0.5,-0.25,0,.25,.5), add=T)
+      if(sm == 1) mtext(paste(i,j,sep=","), side=2)
     }
   }
 }
-
+par(mfrow=c(1,1),mar=c(0,0,0,0),oma=c(3,15,3,0.5))
+image.plot(matrix(rep(zr,2),2,2), legend.only = T, col=heat.colors(100))
+par(mfrow=c(1,1),oma=c(0,0,0,0))
+mtext("Stellar mass value", side=1, line=-2)
 dev.off()
 
